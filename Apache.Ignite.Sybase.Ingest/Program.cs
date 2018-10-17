e9fe5133c3520f7e35e14aef4bfba3e50f08ad88
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-10-17 14:23:48 +0300

    Implement DDL parsing

--- Apache.Ignite.Sybase.Ingest/Program.cs
+++ Apache.Ignite.Sybase.Ingest/Program.cs
@@ -16,20 +16,37 @@ namespace Apache.Ignite.Sybase.Ingest
             // Ideally producer/consumer with back-pressure is required: load data from disk and parse, pass to Streamer.
             // For simplicity let's just have single-threaded method to load single table, then run in parallel for multiple tables.
 
+            ParseDdls();
+        }
+
+        private static void ParseDdls()
+        {
             var dir = Path.GetFullPath(@"..\..\data\ddl");
             var ddlFiles = Directory.GetFiles(dir);
 
             Console.WriteLine($"Found {ddlFiles.Length} DDL files.");
 
-            foreach (var file in ddlFiles)
-            {
-                var tableDef = ParseDdl(file);
+            var tables = ddlFiles
+                .Select(ParseDdl)
+                .Where(t => t != null)
+                .ToArray();
 
-                if (tableDef != null)
-                {
-                    Console.WriteLine($"{tableDef.Name} ({string.Join(", ", tableDef.Columns.Select(c => $"{c.Name}:{c.SqlType}"))})");
-                }
+            foreach (var table in tables)
+            {
+                Console.WriteLine(
+                    $"{table.Name} ({string.Join(", ", table.Columns.Select(c => $"{c.Name}:{c.SqlType}_{c.SqlTypeQualifier}"))})");
             }
+
+            Console.WriteLine("\nDistinct SQL data types:");
+
+            var dataTypes = tables
+                .SelectMany(t => t.Columns)
+                .Select(c => $"{c.SqlType}_{c.SqlTypeQualifier}")
+                .Distinct();
+
+            // TODO: CTL files contain exact positions of all fields.
+            // FIX NNN directives denote the line width
+            Console.WriteLine(string.Join("\n", dataTypes));
         }
 
         private static TableDefinition ParseDdl(string path)
@@ -51,13 +68,13 @@ namespace Apache.Ignite.Sybase.Ingest
                 .Skip(1)
                 .Select(l =>
                 {
-                    var parts = l.Split(new[] {" ", "("}, StringSplitOptions.RemoveEmptyEntries);
+                    var parts = l.Split(new[] {" ", "(", ")"}, StringSplitOptions.RemoveEmptyEntries);
                     if (parts.Length != 3)
                     {
                         return null;
                     }
 
-                    return new ColumnDefinition(parts[0], parts[1]);
+                    return new ColumnDefinition(parts[0], parts[1], parts[2]);
                 })
                 .Where(c => c != null)
                 .ToList();
@@ -81,13 +98,15 @@ namespace Apache.Ignite.Sybase.Ingest
 
     class ColumnDefinition
     {
-        public ColumnDefinition(string name, string sqlType)
+        public ColumnDefinition(string name, string sqlType, string sqlTypeQualifier)
         {
             Name = name;
             SqlType = sqlType;
+            SqlTypeQualifier = sqlTypeQualifier;
         }
 
         public string Name { get;  }
         public string SqlType { get; }
+        public string SqlTypeQualifier { get; }
     }
 }
