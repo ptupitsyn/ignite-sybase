d4d841567c9aa23752e59a704a70f9d6426f96aa
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-10-17 12:49:10 +0300

    Initial commit

---
+++ Apache.Ignite.Sybase.Ingest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Apache.Ignite.Sybase.Ingest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: We should saturate CPU, Network, and Disk for best perf.
+            // Ideally producer/consumer with back-pressure is required: load data from disk and parse, pass to Streamer.
+            // For simplicity let's just have single-threaded method to load single table, then run in parallel for multiple tables.
+        }
+    }
+}
