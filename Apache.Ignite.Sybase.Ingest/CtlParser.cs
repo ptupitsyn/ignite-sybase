23dd88de24a157ea5654aa9935988c3b4faedbda
Pavel Tupitsyn <ptupitsyn@apache.org>
2018-10-18 10:05:26 +0300

    Implement CTL parsing

--- Apache.Ignite.Sybase.Ingest/CtlParser.cs
+++ Apache.Ignite.Sybase.Ingest/CtlParser.cs
@@ -1,20 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Apache.Ignite.Sybase.Ingest
 {
     public static class CtlParser
     {
+        private const string TokenIntoTable = "Into table";
+        private const string TokenInfile = "INFILE";
+
         public static RecordDescriptor ParseCtl(string path)
         {
             var lines = File.ReadAllLines(path);
-            string inFile;
-            int length;
+            var inFile = string.Empty;
+            var tableName = string.Empty;
+            var length = 0;
+            var fields = new List<RecordField>();
 
             foreach (var line in lines)
             {
-                if (line.StartsWith("INFILE", StringComparison.Ordinal))
+                if (line.StartsWith(TokenInfile, StringComparison.Ordinal))
                 {
                     var parts = line.Split(new[] {"'", "\""}, StringSplitOptions.RemoveEmptyEntries);
 
@@ -26,22 +32,38 @@ namespace Apache.Ignite.Sybase.Ingest
                     inFile = parts[1];
                     length = int.Parse(parts[2].Split(" ")[1]);
                 }
+
+                if (line.StartsWith(TokenIntoTable, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    tableName = line
+                        .Substring(TokenInfile.Length)
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .First();
+                }
             }
+
+
+            return fields.Any() && length > 0
+                ? new RecordDescriptor(length, fields, inFile, tableName)
+                : null;
         }
     }
 
     public class RecordDescriptor
     {
-        public RecordDescriptor(int length, IReadOnlyCollection<RecordField> fields, string inFile)
+        public RecordDescriptor(int length, IReadOnlyCollection<RecordField> fields, string inFile, string tableName)
         {
             Length = length;
             Fields = fields;
             InFile = inFile;
+            TableName = tableName;
         }
 
         public int Length { get; }
 
         public string InFile { get; }
+
+        public string TableName { get; }
 
         public IReadOnlyCollection<RecordField> Fields { get; }
     }
